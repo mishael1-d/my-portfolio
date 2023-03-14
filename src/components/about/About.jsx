3f688bc29891c160f96ccf0cf2d7fee16f144e39
Mishael Dada <mishaeldada@gmail.com>
2023-03-14 03:58:26 +0100

    about page mobile responsive

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,31 +2,52 @@ import React from "react";
 import Buttons from "../../UIComponents/Buttons";
 import { motion } from "framer-motion";
 import { DocIcon } from "../../utils/icons";
+import useMediaQuery from "../../hooks/useMediaQuery";
 
 function About() {
+  const isDesktop = useMediaQuery("(min-width: 960px)");
   return (
     <div className="app-container">
-      <div className="relative flex  gap-[6.44rem]">
-        <motion.div
-          initial={{ scale: 0 }}
-          animate={{ scale: 1 }}
-          transition={{ duration: 1 }}
-          className="relative w-[40rem]"
-        >
-          <picture className=" relative z-[1] scale-[.95] ">
-            <img src="/assets/about-img.png" alt="" />
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#363636] z-[-1] scale-[.97] rounded-[1.5rem]"></div>
-          </picture>
-        </motion.div>
+      <div
+        className={`relative flex gap-[6.44rem] ${!isDesktop && "flex-col"}`}
+      >
+        {isDesktop ? (
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 1 }}
+            className={`relative w-[40rem]`}
+          >
+            <picture className=" relative z-[1] scale-[.95] ">
+              <img src="/assets/about-img.png" alt="" />
+              <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#363636] z-[-1] scale-[.97] rounded-[1.5rem]"></div>
+            </picture>
+          </motion.div>
+        ) : null}
         <div className="relative max-w-[64rem] flex flex-col gap-[1.7rem]">
           <motion.h3
             initial={{ y: "-100vh" }}
             animate={{ y: 0 }}
             transition={{ duration: 0.5 }}
-            className="text-black text-[4.8rem]"
+            className={`text-black text-[4.8rem] ${
+              !isDesktop && "text-center"
+            }`}
           >
             About Me
           </motion.h3>
+          {!isDesktop ? (
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 1 }}
+              className={`relative w-full`}
+            >
+              <picture className=" relative z-[1] scale-[.95] ">
+                <img src="/assets/about-img.png" alt="" />
+                <div className="absolute top-0 left-0 right-0 bottom-0 bg-[#363636] z-[-1] scale-[.97] rounded-[1.5rem]"></div>
+              </picture>
+            </motion.div>
+          ) : null}
           <motion.p
             initial={{ x: "100vw" }}
             animate={{ x: 0 }}
@@ -52,7 +73,13 @@ function About() {
             Pupilbase to Hydreate, each project represents a different aspect of
             my abilities and demonstrates my versatility as a Frontend Engineer.
           </motion.p>
-          <Buttons text="download CV" icon={<DocIcon />} />
+          <motion.div
+            initial={{ y: "100vh" }}
+            animate={{ y: 0 }}
+            transition={{ delay: 0.5, duration: 1 }}
+          >
+            <Buttons text="download CV" icon={<DocIcon />} />
+          </motion.div>
         </div>
       </div>
     </div>
--- src/components/bottomNav/BottomNav.jsx
+++ src/components/bottomNav/BottomNav.jsx
@@ -7,6 +7,7 @@ import {
   WorkIcon,
 } from "../../utils/icons";
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function BottomNav() {
   return (
@@ -14,13 +15,23 @@ function BottomNav() {
       initial={{ y: "100vh" }}
       animate={{ y: 0 }}
       transition={{ delay: 1, duration: 1 }}
-      className="flex fixed bottom-0 py-8 border-t-[1px] w-full justify-center gap-[3rem] bg-[#e6e6e6]"
+      className="flex sticky bottom-0 py-8 border-t-[1px] w-full justify-center gap-[3rem] bg-[#e6e6e6]"
     >
-      <HomeIcon />
-      <AboutIcon />
-      <WorkIcon />
-      <BlogIcon />
-      <ContactIcon />
+      <Link to="/">
+        <HomeIcon />
+      </Link>
+      <Link to="/about">
+        <AboutIcon />
+      </Link>
+      <Link to="/">
+        <WorkIcon />
+      </Link>
+      <Link to="/">
+        <BlogIcon />
+      </Link>
+      <Link to="/">
+        <ContactIcon />
+      </Link>
     </motion.div>
   );
 }
