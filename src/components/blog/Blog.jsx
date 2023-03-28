a99e6b71db60606452a24dbdd702877c2935bdc2
Mishael Dada <mishaeldada@gmail.com>
2023-03-28 03:29:41 +0100

    added my own images

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -28,7 +28,7 @@ function Blog() {
           <img
             src="/assets/blog-banner.png"
             alt=""
-            className="relative z-40 opacity-[52%]"
+            className="relative z-40 opacity-[52%] w-full"
           />
           <div className="absolute inset-0 bg-[#363636]  z-30"></div>
           <p className="absolute top-0 bottom-0 right-0 left-0 z-50 text-white flex justify-center items-center font-semibold md:text-[4.8rem] text-[3rem]">
